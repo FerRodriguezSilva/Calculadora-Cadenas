f648702059caea42a12733fb6c8f72d8bbcb8ddf
Fernando Rodriguez <fernando.ale.fars@gmail.com>
2024-11-07 04:31:57 -0400

    Test para que los numeros sean menores a 1000

--- src/Contador_Cad.js
+++ src/Contador_Cad.js
@@ -1,14 +1,26 @@
-function contCadena(cadena) {
+function contCadena(cadena,delimitador) {
 
     if (cadena === "") {
         return 0;
     }
 
-    
-    let partes = cadena.split(/[,|-]/).map(Number);
+    // Definir los delimitadores predeterminados
+  let delimitadores = [",|-"];
+
+  // Si el usuario proporciona un delimitador adicional, lo agregamos
+  if (delimitador) {
+    delimitadores += `|${delimitador}`;
+  }
+
+   // Crear una expresión regular con todos los delimitadores
+  let delimitadorRegex = new RegExp(`[${delimitadores}]`, 'g');
+  
+  // Separar la cadena usando la expresión regular
+  let partes = cadena.split(delimitadorRegex).map(Number);
     let suma_total = 0;
 
     for (let i = 0; i < partes.length; i++) {
+        if(partes[i]<1000)
         suma_total += partes[i];
     }
 
--- src/Contador_Cad.test.js
+++ src/Contador_Cad.test.js
@@ -2,15 +2,21 @@ import contCadena from './Contador_Cad.js'
 
 describe("Cadena Vacia", () => {
     it("si la cadena esta vacia deberia devolver 0", () => {
-      expect(contCadena("")).toEqual(0);
+      expect(contCadena("","")).toEqual(0);
     });
     it('Si la ceda tiene un numero devolver ese numero',()=>{
-        expect(contCadena("2")).toEqual(2);
+        expect(contCadena("2","")).toEqual(2);
     })
     it('Si la cadena tiene ahora mas de un valor',()=>{
-        expect(contCadena("2,3")).toEqual(5);
+        expect(contCadena("2,3","")).toEqual(5);
     })
     it('Si la cadena tiene un diferente separador',()=>{
-        expect(contCadena("2,3-5")).toEqual(10);
+        expect(contCadena("2,3-5","")).toEqual(10);
     })
+   /* it('El usuario puede elegir el delimitador',()=>{
+        expect(contCadena("2,3-5;5",";")).toEqual(15);
+    })*/
+    it('Si la cadena tiene numeros mayores a mil ignorarlos',()=>{
+                expect(contCadena("2,3-5,1002","")).toEqual(10);
+     })
   });
